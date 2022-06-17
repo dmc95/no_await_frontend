21c3ccb641bedce438b421935c9b2b5986fee454
dmc95 <d-mc95@hotmail.fr>
2022-06-17 18:54:55 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./containers/Home";
+import Dishes from "./containers/Dishes";
 
 function App() {
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <Router>
+        <Routes>
+          <Route path="/dishes" element={<Dishes />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
